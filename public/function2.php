9491517b9dc7106877d021abb73fd0a1330a287d
JustMoriah <al222310554@gmail.com>
2024-09-18 20:00:29 -0600

    Function 3 added

---
+++ public/function2.php
@@ -0,0 +1,13 @@
+<?php
+
+class Function2
+{
+    /**
+     * A basic test example.
+     */
+    public function second_fuction(): void
+    {
+        // This is the second function.
+    }
+}
+?>
---
+++ public/function3.php
@@ -0,0 +1,13 @@
+<?php
+
+class Function3
+{
+    /**
+     * A basic test example.
+     */
+    public function third_fuction(): void
+    {
+        // This is the third function.
+    }
+}
+?>
