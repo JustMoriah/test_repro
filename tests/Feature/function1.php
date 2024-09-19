aec0e361b4026690d861514a46069dc8920c3f8c
JustMoriah <al222310554@gmail.com>
2024-09-18 19:33:13 -0600

    Function 1 added

---
+++ tests/Feature/function1.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Feature;
+
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class Function1 extends TestCase
+{
+    /**
+     * A basic test example.
+     */
+    public function first_fuction(): void
+    {
+        // This is the first function.
+    }
+}
